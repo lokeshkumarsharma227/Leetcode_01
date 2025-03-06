f8a34f555a3c47dccc1a5436f33d0d0b6359483f
lokeshkumarsharma227 <128884550+lokeshkumarsharma227@users.noreply.github.com>
2025-03-06 11:56:58 +0530

    Time: 31 ms (63.17%), Space: 20.3 MB (70.14%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        mp = {0: 1} 
+        sum1 = 0
+        ans = 0
+        
+        for num in nums:
+            sum1 += num
+            if (sum1 - k) in mp:
+                ans += mp[sum1 - k]
+            mp[sum1] = mp.get(sum1, 0) + 1
+        
+        return ans
