d7b7b09be885a6efd0eca30fc85f96c59efbb209
lokeshkumarsharma227 <128884550+lokeshkumarsharma227@users.noreply.github.com>
2025-05-11 23:51:47 +0530

    Time: 3 ms (1%), Space: 12 MB (1.15%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count=0;
+        for(int num:arr){
+            if(num%2==0){
+                count=0;
+            }
+            else{
+                count++;
+                if(count==3){
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+};
